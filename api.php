3993f409172ba6e9d74ba5c52e4281fadb5041ad
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-22 20:39:14 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,22 +1,16 @@
 <?php
 
-$ch = curl_init();
-$getid = $_GET['image_url'];
-$data = array('image' => "$getid");
-curl_setopt($ch, CURLOPT_URL, 'https://api.deepai.org/api/nsfw-detector');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+$url = "https://api.ocr.space/Parse/Image";
 
-$headers = array();
-$headers[] = 'Api-Key: 2eafb2bf-243c-4098-9d72-cac2a90d8d2c';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Image url is wrong api by @Sylixx @SylixTeam';
-}
-else {
-    echo $result;
-}
-curl_close($ch);
+$headers = array(
+   "apikey: eb18cc1eca88957",
+   "Content-Type: application/x-www-form-urlencoded",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($curl);
+echo $result;
