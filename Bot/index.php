e1e1fa6355276a37377eb8dbe4afa465e83f4cb5
dsfffe <66359586+dsfffe@users.noreply.github.com>
2021-07-31 23:41:59 +0200

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -62,7 +62,9 @@ $patch = $get->result->file_path;
 $URL = 'https://api.telegram.org/file/bot'.API_KEY.'/'.$patch;
 $detect = json_decode(file_get_contents("https://nsfw-demo.sashido.io/api/image/classify?url=$URL"), true);
     $score = $detect['2'];
-    $kos = $score['probability'];
+  $type = $score['className'];
+$kos = $score['probability'];
+    if ($type == "Porn"){
     $org = "1";
     $math1 = 100;
     $math = $kos * $math1;
@@ -76,6 +78,14 @@ $detect = json_decode(file_get_contents("https://nsfw-demo.sashido.io/api/image/
     }
 
 }
+} else {
+     Alvi('sendMessage',[
+            'chat_id'=>$chat_id,
+            'text'=> "type fail",
+            'reply_to_message_id'=>$message_id,
+            ]); 
+}
+    
 if($msg == "/start" or $msg == "/start@MissAlvi_bot"){
 Alvi('sendMessage',[
 'chat_id'=>$chat_id,
